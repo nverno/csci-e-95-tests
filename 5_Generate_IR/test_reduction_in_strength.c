d7b23a01d3a8b08d7d5169825f6a10055a9712cc
nverno <noah.v.peart@gmail.com>
2018-04-10 12:45:20 -0400

    squash

--- 5_Generate_IR/test_reduction_in_strength.c
+++ 5_Generate_IR/test_reduction_in_strength.c
@@ -1,28 +1,28 @@
-            int read_int(void);
-    void read_string(char *s, int n);
-    void print_int(int i);
-    void print_string(char *s);
-    int atoi(char *s);
-    void strcpy(char *dest, char *src);
-    void print_int_hex(int i);
+int read_int(void);
+void read_string(char *s, int n);
+void print_int(int i);
+void print_string(char *s);
+int atoi(char *s);
+void strcpy(char *dest, char *src);
+void print_int_hex(int i);
 
 
-        int main(int argc, char *argv[]) {
-          int x;
-          x = atoi(argv[1]);
+int main(int argc, char *argv[]) {
+    int x;
+    x = atoi(argv[1]);
 
-          print_string("\nx * 1024 = ");
-          print_int(x * 1024);
+    print_string("\nx * 1024 = ");
+    print_int(x * 1024);
 
-          print_string("\n1024 * x = ");
-          print_int(1024 * x);
+    print_string("\n1024 * x = ");
+    print_int(1024 * x);
 
-          print_string("\nx / 1024 = ");
-          print_int(x / 1024);
+    print_string("\nx / 1024 = ");
+    print_int(x / 1024);
 
-          print_string("\nx % 1024 = ");
-          print_int(x % 1024);
+    print_string("\nx % 1024 = ");
+    print_int(x % 1024);
 
-          print_string("\n");
-          return 0;
-        }
+    print_string("\n");
+    return 0;
+}
