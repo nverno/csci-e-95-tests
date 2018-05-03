91a8850733f57cb568c69007759d902be68f24b7
nverno <noah.v.peart@gmail.com>
2018-05-03 13:18:47 -0400

    squash

--- 6_Generate_MIPS/simple/test_local_vars_lecture.c
+++ 6_Generate_MIPS/simple/test_local_vars_lecture.c
@@ -1,8 +1,10 @@
-int main(void) {       /* if n beginning of offset for locals */
-    int l0i, l0j, l0k; /* l0i @ offset+n, l0j @ offset+n+4, l0k @ offset+n+8 */
-    char l0c1;         /* l0c1 @ offset+n+12 */
-    short l0s1;        /* l0s1 @ offset+n+14 -- NOTE: not +n+13 */
-    short l0s2;        /* l0s2 @ offset+n+16 */
+int main(int argc, char *argv[]) { /* if n beginning of offset for locals */
+    int l0i,                       /* l0i @  offset+n, */
+        l0j,                       /* l0j @  offset+n+4, */
+        l0k;                       /* l0k @  offset+n+8 */
+    char l0c1;                     /* l0c1 @ offset+n+12 */
+    short l0s1;                    /* l0s1 @ offset+n+14 -- NOTE: not +n+13 */
+    short l0s2;                    /* l0s2 @ offset+n+16 */
     {
         int l1i;  /* l1i @ offset+n+20 -- NOTE: not +n+18 */
     }
